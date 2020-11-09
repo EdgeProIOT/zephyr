13daffad6e2c3a2046702740940fc4f13181c838
jturnsek <jernej.turnsek@gmail.com>
2020-11-09 08:30:17 +0100

    Updating nxp_qspi_nor driver

--- drivers/flash/nxp_qspi_nor.c
+++ drivers/flash/nxp_qspi_nor.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Softube AB
+ * Copyright (c) 2020 Jernej Turnsek
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -16,27 +16,10 @@
 #include <soc.h>
 #include <devicetree.h>
 
-#include "spi_nor.h"
-
-#include <logging/log.h>
-LOG_MODULE_REGISTER(flash_qspi, LOG_LEVEL_DBG);
-
 #include "fsl_flexspi.h"
 
-struct flash_priv {
-	struct k_sem write_lock;
-	FLEXSPI_Type  *pflash_block_base;
-};
-
-static const struct flash_parameters
-	flash_mcux_flexspi_qspi_flash_parameters = {
-	.write_block_size = 4,
-	.erase_value = 0xff,
-};
-
-/* Get the ram buffer size from the flexspi node */
-#define FLASH_WRITE_SIZE DT_PROP(DT_INST(0, nxp_imx_flexspi), \
-				 zephyr_ram_buffer_size)
+#define FLASH_SECTOR_SIZE 	4096
+#define FLASH_PAGE_SIZE 	256
 
 #define CUSTOM_LUT_LENGTH 60
 #define FLASH_QUAD_ENABLE 0x40
@@ -44,11 +27,84 @@ static const struct flash_parameters
 #define FLASH_BUSY_STATUS_OFFSET 0
 #define FLASH_ERROR_STATUS_MASK 0x0e
 
-#define NOR_CMD_LUT_SEQ_IDX_READ_FAST_QUAD 0
-#define NOR_CMD_LUT_SEQ_IDX_READSTATUS 1
-#define NOR_CMD_LUT_SEQ_IDX_WRITEENABLE 2
-#define NOR_CMD_LUT_SEQ_IDX_ERASESECTOR 3
-#define NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_QUAD 4
+#define NOR_CMD_LUT_SEQ_IDX_READ_NORMAL        0
+#define NOR_CMD_LUT_SEQ_IDX_READ_FAST          1
+#define NOR_CMD_LUT_SEQ_IDX_READ_FAST_QUAD     2
+#define NOR_CMD_LUT_SEQ_IDX_READSTATUS         3
+#define NOR_CMD_LUT_SEQ_IDX_WRITEENABLE        4
+#define NOR_CMD_LUT_SEQ_IDX_ERASESECTOR        5
+#define NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_SINGLE 6
+#define NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_QUAD   7
+#define NOR_CMD_LUT_SEQ_IDX_READID             8
+#define NOR_CMD_LUT_SEQ_IDX_WRITESTATUSREG     9
+#define NOR_CMD_LUT_SEQ_IDX_ENTERQPI           10
+#define NOR_CMD_LUT_SEQ_IDX_EXITQPI            11
+#define NOR_CMD_LUT_SEQ_IDX_READSTATUSREG      12
+
+const uint32_t customLUT[CUSTOM_LUT_LENGTH] = {
+    /* Normal read mode -SDR */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_NORMAL] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x03, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_NORMAL + 1] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_READ_SDR, kFLEXSPI_1PAD, 0x04, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Fast read mode - SDR */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_FAST] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x0B, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_FAST + 1] = FLEXSPI_LUT_SEQ(
+        kFLEXSPI_Command_DUMMY_SDR, kFLEXSPI_1PAD, 0x08, kFLEXSPI_Command_READ_SDR, kFLEXSPI_1PAD, 0x04),
+
+    /* Fast read quad mode - SDR */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_FAST_QUAD] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x6B, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+    [4 * NOR_CMD_LUT_SEQ_IDX_READ_FAST_QUAD + 1] = FLEXSPI_LUT_SEQ(
+        kFLEXSPI_Command_DUMMY_SDR, kFLEXSPI_4PAD, 0x08, kFLEXSPI_Command_READ_SDR, kFLEXSPI_4PAD, 0x04),
+
+    /* Read extend parameters */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READSTATUS] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x81, kFLEXSPI_Command_READ_SDR, kFLEXSPI_1PAD, 0x04),
+
+    /* Write Enable */
+    [4 * NOR_CMD_LUT_SEQ_IDX_WRITEENABLE] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x06, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Erase Sector  */
+    [4 * NOR_CMD_LUT_SEQ_IDX_ERASESECTOR] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0xD7, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+
+    /* Page Program - single mode */
+    [4 * NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_SINGLE] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x02, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+    [4 * NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_SINGLE + 1] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_WRITE_SDR, kFLEXSPI_1PAD, 0x04, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Page Program - quad mode */
+    [4 * NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_QUAD] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x32, kFLEXSPI_Command_RADDR_SDR, kFLEXSPI_1PAD, 0x18),
+    [4 * NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_QUAD + 1] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_WRITE_SDR, kFLEXSPI_4PAD, 0x04, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Read ID */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READID] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x9F, kFLEXSPI_Command_READ_SDR, kFLEXSPI_1PAD, 0x04),
+
+    /* Enable Quad mode */
+    [4 * NOR_CMD_LUT_SEQ_IDX_WRITESTATUSREG] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x01, kFLEXSPI_Command_WRITE_SDR, kFLEXSPI_1PAD, 0x04),
+
+    /* Enter QPI mode */
+    [4 * NOR_CMD_LUT_SEQ_IDX_ENTERQPI] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x35, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Exit QPI mode */
+    [4 * NOR_CMD_LUT_SEQ_IDX_EXITQPI] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_4PAD, 0xF5, kFLEXSPI_Command_STOP, kFLEXSPI_1PAD, 0),
+
+    /* Read status register */
+    [4 * NOR_CMD_LUT_SEQ_IDX_READSTATUSREG] =
+        FLEXSPI_LUT_SEQ(kFLEXSPI_Command_SDR, kFLEXSPI_1PAD, 0x05, kFLEXSPI_Command_READ_SDR, kFLEXSPI_1PAD, 0x04),
+};
+
 
 flexspi_device_config_t deviceconfig = {
 	.flexspiRootClk = DT_INST_PROP(0, spi_max_frequency),
@@ -68,7 +124,19 @@ flexspi_device_config_t deviceconfig = {
 	.AHBWriteWaitInterval = 0,
 };
 
-static status_t flash_mcux_flexspi_qspi_wait_bus_busy(struct device *dev,
+struct flash_priv {
+	struct k_sem write_lock;
+	FLEXSPI_Type  *pflash_block_base;
+};
+
+static const struct flash_parameters
+	flash_mcux_flexspi_qspi_flash_parameters = {
+	.write_block_size = 4,
+	.erase_value = 0xff,
+};
+
+
+static status_t flash_mcux_flexspi_qspi_wait_bus_busy(const struct device *dev,
 						      off_t offset)
 {
 	bool is_busy = true;
@@ -83,7 +151,7 @@ static status_t flash_mcux_flexspi_qspi_wait_bus_busy(struct device *dev,
 	flash_transfer.port = kFLEXSPI_PortA1;
 	flash_transfer.cmdType = kFLEXSPI_Read;
 	flash_transfer.SeqNumber = 1;
-	flash_transfer.seqIndex = NOR_CMD_LUT_SEQ_IDX_READSTATUS;
+	flash_transfer.seqIndex = NOR_CMD_LUT_SEQ_IDX_READSTATUSREG;
 	flash_transfer.data = &read_value;
 	flash_transfer.dataSize = 1;
 
@@ -106,7 +174,7 @@ static status_t flash_mcux_flexspi_qspi_wait_bus_busy(struct device *dev,
 	return status;
 }
 
-status_t flash_mcux_flexspi_qspi_write_enable(struct device *dev, off_t offset)
+status_t flash_mcux_flexspi_qspi_write_enable(const struct device *dev, off_t offset)
 {
 	status_t status;
 	flexspi_transfer_t flash_transfer;
@@ -125,7 +193,42 @@ status_t flash_mcux_flexspi_qspi_write_enable(struct device *dev, off_t offset)
 	return status;
 }
 
-static int flash_mcux_flexspi_qspi_read(struct device *dev, off_t offset,
+status_t flash_mcux_flexspi_enable_quad_mode(const struct device *dev, off_t offset)
+{
+    flexspi_transfer_t flashXfer;
+    status_t status;
+    uint32_t writeValue = 0x40;
+
+    struct flash_priv *priv = dev->data;
+    FLEXSPI_Type *base_address = priv->pflash_block_base;
+
+    /* Write neable */
+    status = flash_mcux_flexspi_qspi_write_enable(dev, offset);
+
+    if (status != kStatus_Success) {
+        return status;
+    }
+
+    /* Enable quad mode. */
+    flashXfer.deviceAddress = offset;
+    flashXfer.port          = kFLEXSPI_PortA1;
+    flashXfer.cmdType       = kFLEXSPI_Write;
+    flashXfer.SeqNumber     = 1;
+    flashXfer.seqIndex      = NOR_CMD_LUT_SEQ_IDX_WRITESTATUSREG;
+    flashXfer.data          = &writeValue;
+    flashXfer.dataSize      = 1;
+
+    status = FLEXSPI_TransferBlocking(base_address, &flashXfer);
+    if (status != kStatus_Success) {
+        return status;
+    }
+
+    status = flash_mcux_flexspi_qspi_wait_bus_busy(dev, offset);
+
+    return status;
+}
+
+static int flash_mcux_flexspi_qspi_read(const struct device *dev, off_t offset,
 					void *data, size_t len)
 {
 	status_t status;
@@ -150,18 +253,13 @@ static int flash_mcux_flexspi_qspi_read(struct device *dev, off_t offset,
 	return status;
 }
 
-static int flash_mcux_flexspi_qspi_write(struct device *dev, off_t offset,
+static int flash_mcux_flexspi_qspi_write(const struct device *dev, off_t offset,
 					 const void *data, size_t len)
 {
 	status_t status = kStatus_Fail;
 	flexspi_transfer_t flash_transfer;
 
-/* Only declare if we need to copy writes to ram */
-#if(NXP_QSPI_NOR_RAM_WRITE_BUFFER_SIZE > 0)
-	static uint8_t ram_buffer[FLASH_WRITE_SIZE];
-#endif
-
-	int sectors = len / FLASH_WRITE_SIZE;
+	int pages = len / FLASH_PAGE_SIZE;
 	struct flash_priv *priv = dev->data;
 	FLEXSPI_Type *base_address = priv->pflash_block_base;
 
@@ -169,21 +267,17 @@ static int flash_mcux_flexspi_qspi_write(struct device *dev, off_t offset,
 		return -EACCES;
 	}
 
-	if (len % FLASH_WRITE_SIZE) {
-		sectors++;
+	if (len % FLASH_PAGE_SIZE) {
+		pages++;
 	}
 
-	for (int i = 0; i < sectors; i++) {
-		off_t offset_to_sector = offset + i * FLASH_WRITE_SIZE;
+	for (int i = 0; i < pages; i++) {
+		off_t offset_to_sector = offset + i * FLASH_PAGE_SIZE;
 		uint32_t *offset_to_data =
-			(uint32_t *)((uint8_t *)data + i * FLASH_WRITE_SIZE);
+			(uint32_t *)((uint8_t *)data + i * FLASH_PAGE_SIZE);
 		uint32_t bytes_to_write =
-			len - i * FLASH_WRITE_SIZE >= FLASH_WRITE_SIZE ?
-			FLASH_WRITE_SIZE : len - i * FLASH_WRITE_SIZE;
-
-#if(NXP_QSPI_NOR_RAM_WRITE_BUFFER_SIZE > 0)
-		memcpy(ram_buffer, offset_to_data, bytes_to_write);
-#endif
+			len - i * FLASH_PAGE_SIZE >= FLASH_PAGE_SIZE ?
+			FLASH_PAGE_SIZE : len - i * FLASH_PAGE_SIZE;
 
 		status = flash_mcux_flexspi_qspi_write_enable(dev, offset);
 		if (status == kStatus_Success) {
@@ -192,11 +286,7 @@ static int flash_mcux_flexspi_qspi_write(struct device *dev, off_t offset,
 			flash_transfer.cmdType = kFLEXSPI_Write;
 			flash_transfer.SeqNumber = 1;
 			flash_transfer.seqIndex = NOR_CMD_LUT_SEQ_IDX_PAGEPROGRAM_QUAD;
-#if(NXP_QSPI_NOR_RAM_WRITE_BUFFER_SIZE > 0)
-			flash_transfer.data = (uint32_t *)&ram_buffer[0];
-#else
 			flash_transfer.data = offset_to_data,
-#endif
 			flash_transfer.dataSize = bytes_to_write;
 
 			status = FLEXSPI_TransferBlocking(base_address, &flash_transfer);
@@ -211,7 +301,7 @@ static int flash_mcux_flexspi_qspi_write(struct device *dev, off_t offset,
 	return status;
 }
 
-static int flash_mcux_flexspi_qspi_erase(struct device *dev,
+static int flash_mcux_flexspi_qspi_erase(const struct device *dev,
 					 off_t offset, size_t len)
 {
 	status_t status = kStatus_Fail;
@@ -224,14 +314,14 @@ static int flash_mcux_flexspi_qspi_erase(struct device *dev,
 		return -EACCES;
 	}
 
-	int sectors = len / 4096;
-	if (len % 4096) {
+	int sectors = len / FLASH_SECTOR_SIZE;
+	if (len % FLASH_SECTOR_SIZE) {
 		sectors++;
 	}
 
 	for (int i = 0; i < sectors; i++) {
 		off_t offset_to_sector =
-			offset + i * 4096;
+			offset + i * FLASH_SECTOR_SIZE;
 
 		status = flash_mcux_flexspi_qspi_write_enable(dev, offset);
 		if (status == kStatus_Success) {
@@ -256,7 +346,7 @@ static int flash_mcux_flexspi_qspi_erase(struct device *dev,
 /* Write/erase operations in this driver is protected by a semaphore. This
  * prevents access from multiple threads, but using this function the semaphore
  * can be locked, preventing all write/erase operations. */
-static int flash_mcux_flexspi_qspi_write_protection(struct device *dev,
+static int flash_mcux_flexspi_qspi_write_protection(const struct device *dev,
 						    bool enable)
 {
 	struct flash_priv *priv = dev->data;
@@ -273,12 +363,11 @@ static int flash_mcux_flexspi_qspi_write_protection(struct device *dev,
 
 #if defined(CONFIG_FLASH_PAGE_LAYOUT)
 static const struct flash_pages_layout dev_layout = {
-	.pages_count = KB(CONFIG_FLASH_SIZE) /
-		       4096,
-	.pages_size = 4096,
+	.pages_count = KB(CONFIG_FLASH_SIZE) / FLASH_SECTOR_SIZE,
+	.pages_size = FLASH_SECTOR_SIZE,
 };
 
-static void flash_mcux_flexspi_qspi_pages_layout(struct device *dev,
+static void flash_mcux_flexspi_qspi_pages_layout(const struct device *dev,
 						 const struct flash_pages_layout **layout,
 						 size_t *layout_size)
 {
@@ -327,18 +416,18 @@ static int flash_mcux_flexspi_qspi_init(const struct device *dev)
 	FLEXSPI_GetDefaultConfig(&config);
 
 	/* Set AHB buffer size for reading data through AHB bus. */
-	config.ahbConfig.enableAHBPrefetch = true;
-	config.ahbConfig.enableAHBBufferable = true;
-	config.ahbConfig.enableReadAddressOpt = true;
-	config.ahbConfig.enableAHBCachable = true;
-	config.rxSampleClock = kFLEXSPI_ReadSampleClkLoopbackInternally;
-	FLEXSPI_Init(base, &config);
+    config.ahbConfig.enableAHBPrefetch = true;
+    config.rxSampleClock               = kFLEXSPI_ReadSampleClkLoopbackFromDqsPad;
+    FLEXSPI_Init(base, &config);
 
-	/* Configure flash settings according to serial flash feature. */
-	FLEXSPI_SetFlashConfig(base, &deviceconfig, kFLEXSPI_PortA1);
+    /* Configure flash settings according to serial flash feature. */
+    FLEXSPI_SetFlashConfig(base, &deviceconfig, kFLEXSPI_PortA1);
 
-	/* Do software reset. */
-	FLEXSPI_SoftwareReset(base);
+    /* Update LUT table. */
+    FLEXSPI_UpdateLUT(base, 0, customLUT, CUSTOM_LUT_LENGTH);
+
+    /* Enter quad mode. */
+    flash_mcux_flexspi_enable_quad_mode(dev, 0);
 
 	k_sem_init(&priv->write_lock, 0, 1);
 
